0d4bdf2b3c8f1fe2f7ff8169cc84deefb5238e3e
songfanglin <songfanglin@fenbi.com>
2020-07-08 16:31:42 +0800

    change models

--- app_01/models.py
+++ app_01/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-is_leave = (("0", "否"), ("1", "是"))
+is_leave = ((0, "否"), (1, "是"))
 
 
 class Oaapprover(models.Model):
     ldap = models.CharField('ldap', primary_key=True, max_length=255)
     name = models.CharField('姓名', max_length=255, blank=False, null=True)
-    leave = models.CharField('能否审批请假', choices=is_leave, max_length=255, blank=False, null=True,default='0')
-    dimission = models.CharField('能否审批离职权限', choices=is_leave, max_length=255, blank=False, null=True,default='0')
+    leave = models.IntegerField('能否审批请假', choices=is_leave, blank=False, null=True,default=0)
+    dimission = models.IntegerField('能否审批离职权限', choices=is_leave, blank=False, null=True,default=0)
 
     def __str__(self):
         return "审批权限表---->" + str(self.name)
@@ -48,11 +48,11 @@ class Oahr(models.Model):
     ldap = models.CharField(primary_key=True, max_length=255)
     name = models.CharField('姓名', max_length=255, blank=False, null=True)
     company = models.CharField('可导出员工信息的公司名称', max_length=255, blank=False, null=True)
-    exportstaff = models.CharField('能否导出员工', choices=is_leave, db_column='exportStaff', max_length=255, blank=False,
-                                   null=True,default='0')  # Field name made lowercase.
-    confirmentry = models.CharField('能否确定入职', choices=is_leave, db_column='confirmEntry', max_length=255, blank=False,
-                                    null=True,default='0')  # Field name made lowercase.
-    cancel_ack_leave_auth = models.CharField('能否销假', choices=is_leave, max_length=255, blank=False, null=True,default='0')
+    exportstaff = models.IntegerField('能否导出员工', choices=is_leave, db_column='exportStaff', blank=False,
+                                   null=True,default=0)  # Field name made lowercase.
+    confirmentry = models.IntegerField('能否确定入职', choices=is_leave, db_column='confirmEntry', blank=False,
+                                    null=True,default=0)  # Field name made lowercase.
+    cancel_ack_leave_auth = models.IntegerField('能否销假', choices=is_leave, blank=False, null=True,default=0)
 
     def __str__(self):
         return "人事权限表---->" + str(self.name)
@@ -66,9 +66,9 @@ class Oahr(models.Model):
 class Oaother(models.Model):
     ldap = models.CharField(primary_key=True, max_length=255)
     name = models.CharField('姓名', max_length=255, blank=True, null=True)
-    qr_code_auth = models.CharField('能否重置二维码', choices=is_leave, max_length=255, blank=False, null=True,default='0')
-    password_auth = models.CharField('能否重置密码', choices=is_leave, max_length=255, blank=False, null=True,default='0')
-    add_ldap_auth = models.CharField('能否添加ldap', choices=is_leave, max_length=255, blank=False, null=True,default='0')
+    qr_code_auth = models.IntegerField('能否重置二维码', choices=is_leave,  blank=False, null=True,default=0)
+    password_auth = models.IntegerField('能否重置密码', choices=is_leave, blank=False, null=True,default=0)
+    add_ldap_auth = models.IntegerField('能否添加ldap', choices=is_leave, blank=False, null=True,default=0)
 
     # cancel_ack_leave_auth = models.CharField('能否销假',max_length=255, blank=True, null=True)
 
