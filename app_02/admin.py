30ca04fff54024e38ea058e22117969cd697757e
songfanglin <songfanglin@fenbi.com>
2020-07-17 17:28:29 +0800

    add pagecount limit

--- app_02/admin.py
+++ app_02/admin.py
@@ -6,20 +6,24 @@ from .models import OaWorkFlow, OaCondition, OaTransferring, OaApproverGroup
 class OaWorkFlowAdmin(admin.ModelAdmin):
     search_fields = ("flowname",)
     list_display = ("id", "flowname", "TID", "tid", "flowtype")
+    list_per_page = 15
 
 
 class OaConditionAdmin(admin.ModelAdmin):
     search_fields = ("descripion", "afid")
     list_display = ('id', 'descripion', 'afid', 'WorkFid', 'workfid', 'priority')
+    list_per_page = 15
 
 
 class OaTransferringAdmin(admin.ModelAdmin):
     list_display = ('id', 'route')
+    list_per_page = 15
 
 
 class OaApproverGroupAdmin(admin.ModelAdmin):
     search_fields = ('groupname','alias','leader')
     list_display = ('id', 'groupname', 'leader', 'order', 'alias', 'must')
+    list_per_page = 15
 
 
 admin.site.register(OaWorkFlow, OaWorkFlowAdmin)
